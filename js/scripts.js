e562d9002c5fe1cdccc1063cbf03d7303e4770bc
Jawad <shaikhjawad007@gmail.com>
2017-06-24 02:27:52 +0530

    client section added

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,21 @@ jQuery(document).ready(function($) {
         }
 	});
 
+
+    $(".speaker_list").owlCarousel({
+    loop:false,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
+})
+
 });
